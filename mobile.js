f3aee468667828a5fe373bf0e6c1f1cc5eca6973
xullant <mareczech93@gmail.com>
2017-05-02 23:57:31 +0200

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
 if ( ($( window).width() < 680 ) ) {
-      document.location = "http://xullant.github.io/mobile.html";
+      document.location = "https://github.com/xullant/metro2033praha/blob/gh-pages/mobile.html";
    }
 });

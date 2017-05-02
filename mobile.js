43cc3c7cbfdb3d973991b411a14f2d3c3b53adda
xullant <mareczech93@gmail.com>
2017-05-02 23:59:30 +0200

    Delete mobile.js

--- mobile.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function($) {
-if ( ($( window).width() < 680 ) ) {
-      document.location = "https://github.com/xullant/metro2033praha/blob/gh-pages/mobile.html";
-   }
-});
